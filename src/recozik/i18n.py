2ae697aec7b43c601e64a46727f2e3f617df6996
Patrick ZAJDA <patrick@zajda.fr>
2025-11-08 18:55:56 +0100

    Include cli module in mypy checks
    Signed-off-by: Patrick ZAJDA <patrick@zajda.fr>

--- src/recozik/i18n.py
+++ src/recozik/i18n.py
@@ -3,7 +3,12 @@
 from __future__ import annotations
 
 import sys
+from typing import TYPE_CHECKING, Any
 
 from recozik_core import i18n as _core_i18n
+
+if TYPE_CHECKING:  # pragma: no cover - typing aid
+    _: Any
+    set_locale: Any
 
 sys.modules[__name__] = _core_i18n
